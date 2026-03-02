88804d89fc8c7685b3b02d855284c7a9d65d962d
thanuja-reddy06 <lekkalathanuja7@gmail.com>
2026-03-02 14:41:12 +0530

    Task3_Sudoku_Solver.py

---
+++ Task3_Sudoku_Solver.py
@@ -0,0 +1,67 @@
+def print_board(board):
+    for row in board:
+        print(row)
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+def is_valid(board, num, pos):
+    row, col = pos
+
+    # Check row
+    for i in range(9):
+        if board[row][i] == num and col != i:
+            return False
+
+    # Check column
+    for i in range(9):
+        if board[i][col] == num and row != i:
+            return False
+
+    # Check 3x3 box
+    box_x = col // 3
+    box_y = row // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+def solve(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+solve(board)
+print_board(board)
